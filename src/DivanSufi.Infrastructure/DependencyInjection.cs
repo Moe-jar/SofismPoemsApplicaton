254e72a34cf9f43b0dab1dd8ac7d8ff28ffb8b6d
MOHAMMAD QASEM JARADAT <mqjaradat21@cit.just.edu.jo>
2026-03-26 17:38:20 +0300

    some fixes

--- src/DivanSufi.Infrastructure/DependencyInjection.cs
+++ src/DivanSufi.Infrastructure/DependencyInjection.cs
@@ -12,8 +12,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-         var connectionString = NeonPostgresSetup.ResolveConnectionString(configuration);
-
+           var connectionString = NeonPostgresSetup.ResolveConnectionString(configuration);
         services.AddDbContext<AppDbContext>(options =>
         {
             
